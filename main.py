be113eb2faa28e76d22fa5bb7fe65783085878d4
Sannidhya Dasgupta <72875652+Sannidhya4396@users.noreply.github.com>
2020-12-20 16:52:51 +0530

    Working on functions. I decided to take some break between this project development and study pygithub module more closely. No updates expected for the next 3-4 days in this repo. Will come back after studying pygithub. Bye

--- main.py
+++ main.py
@@ -8,14 +8,31 @@ import ctypes
 import win32event
 import win32process
 import sys
+from colored import fg, bg, attr
+
+
+def repoLister(user):
+    f = open(r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI\username.txt")
+    texts = f.read()
+    f.close()
+    allr = user.get_user().get_repos()
+    for repo in user.get_repos():
+        print(repo.name)
 
 
 if __name__ == "__main__":
     Log = os.path.exists(
         r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI\username.txt")
     if Log == False:
+        global username
+        global password
         username = input("Enter username of GitHub account: ")
         password = input("Enter password: ")
+        global g
+        global user
+        g = Github(username, password)
+        user = g.get_user()
+        # user.login
         try:
             g = Github(username, password)
             user = g.get_user(username)
@@ -30,12 +47,31 @@ if __name__ == "__main__":
         except Exception as e:
             print(e)
     else:
-        while True:
-            mainPath = os.getcwd()
-            comd = input(f"{mainPath}: ")
-            if comd == "help":
-                print("No help available")
-            elif comd == "exit":
-                exit()
-            else:
-                print(f"bash: {comd}: no such command")
+        try:
+            UserLogged = open(
+                r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI\username.txt", "r")
+            UserPass = open(
+                r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI\userpass.txt", "r")
+            name = UserLogged.read()
+            password = UserPass.read()
+            print(name)
+            print(password)
+            # g = Github(name, password)
+            # user = g.get_user()
+            # user.login
+            while True:
+                mainPath = os.getcwd()
+                comd = input(f"{fg('green_1')}{mainPath}: {attr('reset')}")
+                if comd == "help" or comd == "help me":
+                    print("No help available")
+                elif comd == "lsr":
+                    repoLister(user.name)
+
+                elif comd == "exit":
+                    exit()
+                else:
+                    print(
+                        f"{fg('red_1')}bash: '{comd}': no such command{attr('reset')}")
+
+        except Exception as e:
+            print(e)
