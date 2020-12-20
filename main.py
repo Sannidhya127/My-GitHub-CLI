8b9757d12d223554cd077f75bcfad446923568ca
Sannidhya Dasgupta <72875652+Sannidhya4396@users.noreply.github.com>
2020-12-20 14:26:02 +0530

    Made the first step of user log in. Now will start making the important functions like creation of repos, making commits, etc

--- main.py
+++ main.py
@@ -2,22 +2,34 @@ import github
 from github import Github
 import win32api
 import win32con
+import win32com
 import os
+import ctypes
+import win32event
+import win32process
+import sys
+
 
 if __name__ == "__main__":
-    username = input("Enter username of GitHub account: ")
-    password = input("Enter password: ")
-    try:
-		g = Github(username, password)
-		user = g.get_user(username)
-		user.login
-		UserLogged = open("username", "w")
-		UserLogged = open("userpass", "w")
-		win32api.SetFileAttributes(
-			"username", win32con.FILE_ATTRIBUTE_HIDDEN)
-		win32api.SetFileAttributes(
-			"userpass", win32con.FILE_ATTRIBUTE_HIDDEN)
-	except Exception:
-		print("Error loggin in. Try again later")
+    Log = os.path.exists(
+        r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI\username.txt")
+    if Log == False:
+        username = input("Enter username of GitHub account: ")
+        password = input("Enter password: ")
+        try:
+            g = Github(username, password)
+            user = g.get_user(username)
+            user.login
+            os.mkdir(r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI")
+            UserLogged = open(
+                r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI\username.txt", "w")
+            UserPass = open(
+                r"C:\Users\KAUSTAV\Documents\CrT GitHub CLI\userpass.txt", "w")
+            UserLogged.write(username)
+            UserPass.write(password)
+        except Exception as e:
+            print(e)
+    else:
+        print("All set")
     # for repo in user.get_repos():
     #     print(repo.name)
